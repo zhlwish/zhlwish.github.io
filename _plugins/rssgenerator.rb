ac3864ef8c53634bce4ec50d9070c6e3c69e677a
Zhou Liang <zhlwish@gmail.com>
2014-05-23 00:05:13 +0800

    fix rss

--- _plugins/rssgenerator.rb
+++ _plugins/rssgenerator.rb
@@ -18,7 +18,7 @@
 # Source: http://github.com/agelber/jekyll-rss
 #
 # Distributed under the MIT license
-# Copyright Assaf Gelber 2014
+# Copyright Assaf Gelber 2013
 
 module Jekyll
   class RssFeed < Page; end
@@ -35,6 +35,8 @@ module Jekyll
     def generate(site)
       require 'rss'
 
+      parser = get_markdown_parser(site.config)
+
       # Create the rss with the help of the RSS module
       rss = RSS::Maker.make("2.0") do |maker|
         maker.channel.title = site.config['name']
@@ -47,13 +49,10 @@ module Jekyll
         post_limit = (site.config['rss_post_limit'] - 1 rescue site.posts.count)
 
         site.posts.reverse[0..post_limit].each do |post|
-          post.render(site.layouts, site.site_payload)
           maker.items.new_item do |item|
-            link = "#{site.config['url']}#{post.url}"
-            item.guid.content = link
             item.title = post.title
-            item.link = link
-            item.description = post.excerpt
+            item.link = "#{site.config['url']}#{post.url}"
+            item.description = parser.convert(post.excerpt)
             item.updated = post.date
           end
         end
@@ -62,10 +61,9 @@ module Jekyll
       # File creation and writing
       rss_path = ensure_slashes(site.config['rss_path'] || "/")
       rss_name = site.config['rss_name'] || "rss.xml"
-      full_path = File.join(site.dest, rss_path)
+      full_path = File.join(site.source, rss_path)
       ensure_dir(full_path)
       File.open("#{full_path}#{rss_name}", "w") { |f| f.write(rss) }
-
       # Add the feed page to the site pages
       site.pages << Jekyll::RssFeed.new(site, site.dest, rss_path, rss_name)
     end
@@ -107,5 +105,28 @@ module Jekyll
     def ensure_dir(path)
       FileUtils.mkdir_p(path)
     end
+
+    # Gets a parser object for the parser specified in the configuration
+    #
+    # config - the site's configuration hash
+    #
+    # Returns a parser or raises exception if one isn't found
+    def get_markdown_parser(config)
+      return case config['markdown']
+        when 'redcarpet'
+          Jekyll::Converters::Markdown::RedcarpetParser.new config
+        when 'kramdown'
+          Jekyll::Converters::Markdown::KramdownParser.new config
+        when 'rdiscount'
+          Jekyll::Converters::Markdown::RDiscountParser.new config
+        when 'maruku'
+          Jekyll::Converters::Markdown::MarukuParser.new config
+        else
+          STDERR.puts "Invalid Markdown processor: #{config['markdown']}"
+          STDERR.puts "  Valid options are [ maruku | rdiscount | kramdown | redcarpet ]"
+          raise FatalException.new("Invalid Markdown process: #{config['markdown']}")
+      end
+    end
+
   end
 end
